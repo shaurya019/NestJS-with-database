060b06d869101e778fd415d3cbcfd2b2b838b550
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-03-27 22:42:52 +0530

    Crud Operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  HttpException,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './users.service';
 import { CreateUserDto } from './dto/CreateUser.dto';
+import mongoose from 'mongoose';
+import { UpdateUserDto } from './dto/UpdateUser.dto';
 
 Controller('users');
 export class UsersController {
@@ -17,6 +28,40 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.userService.getUsers();
+  }
+
+  //   Get user by id
+  // users/:id
+  // it will be a async function
+  @Get(':id')
+  async getUsersById(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('Id Invalid', 404);
+    const findUser = await this.userService.getUserById(id);
+    if (!findUser) throw new HttpException('User not found', 404);
+    return findUser;
+  }
+
+  //   Update user
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('Id Invalid', 404);
+    const updateuser = await this.userService.updateUser(id, updateUserDto);
+    if (!updateuser) throw new HttpException('User not found', 404);
+    return updateuser;
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('Id Invalid', 404);
+    const deletedUser = await this.userService.deleteUser(id);
+    if (!deletedUser) throw new HttpException('User not found', 404);
+    return;
   }
 }
 
